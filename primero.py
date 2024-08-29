5cf21b53ef6594be2f1705ecb3e3ed7b5d1b1b17
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-08-29 18:01:56 -0400

    Cambios en los datos de los intervalos con comentarios

--- primero.py
+++ primero.py
@@ -9,28 +9,32 @@ from pprint import pprint
 sns.set_theme(style="ticks")
 
 # diamonds = sns.load_dataset("diamonds")
-# print(diamonds)
+diamonds = []
 cervezas = []
 
 with open('BEER_proyecto.csv', newline='') as csvfile:
-    # diamonds = pd.DataFrame(lista, columns=cabeza)
     partes = csv.DictReader(csvfile, delimiter=';')
     for esto in partes: 
         esto['PrecioDolares'] = float(esto['PrecioDolares'])
         esto['Calorias'] = float(esto['Calorias'])
-        esto['PorcAlcohol'] = float(esto['PorcAlcohol'])
+        if float(esto['PorcAlcohol']) == 0.0: 
+            esto['PorcAlcohol'] = 0.01
+        else: 
+            esto['PorcAlcohol'] = float(esto['PorcAlcohol'])
         cervezas.append(esto)
-
-    pprint(cervezas)
+    diamonds = pd.DataFrame(cervezas)
 
 def precios(): 
     todos = procedimiento('PrecioDolares')
-    # mostrar_tabla(todos)
+    mostrar_tabla(todos)
 
 def calorias(): 
     todos = procedimiento('Calorias')
-    mostrar_tabla(todos)
+    # mostrar_tabla(todos)
 
+# Por la forma de datos usar datos no agrupados
+# El 0 para python lo considera inexistente por eso se le tuvo que hacer una
+# Pequeña modificación muy cerca del 0 
 def alcohol(): 
     todos = procedimiento('PorcAlcohol')
     # mostrar_tabla(todos)
@@ -40,41 +44,55 @@ def procedimiento(busqueda):
     minimo = min(r)
     maximo = max(r)
     n = len(r)
-    rango = maximo - minimo 
-    k = 1 + 3.3 * math.log(n, 10)
-    amplitud = rango / k
-    if amplitud > int(amplitud): 
-        amplitud += 1
-        amplitud = int(amplitud)
-    lista = {}
-    for esto in r: 
-        try: lista[esto] += 1
-        except: lista[esto] = 1
-    real = []
-    para = 0
-    while True: 
-        if minimo > maximo: break
-        para = minimo + amplitud - 1
-        clase = {
-            'minimo': minimo, 
-            'maximo': para
-        }
-        minimo = para + 1
-        real.append(clase)
-    oficial = [{'clase': real}]
-    oficial[0]['fi'] = []
-    for esto in real: 
-        numero = 0
-        for i in range(int(esto['minimo']), int(esto['maximo']) + 1): 
-            try: numero += lista[i]
-            except: continue
-        oficial[0]['fi'].append(numero)
-    parte = []
-    for esto in oficial[0]['fi']: 
-        if len(parte) >= 1: parte.append(esto + parte[-1])
-        else: parte.append(esto)
-    oficial[0]['fa'] = parte
-    oficial[0]['xi'] = list(map(lambda x: (x['minimo'] + x['maximo']) / 2, oficial[0]['clase']))
+    print(f'Cantidad: {n}')
+    oficial = []
+    if n >= 25 and n <= 400:
+    # if True:  
+        rango = maximo - minimo 
+        k = 1 + 3.3 * math.log(n, 10)
+        amplitud = rango / k
+        if amplitud > float(amplitud): 
+            amplitud += 1
+            amplitud = float(amplitud)
+        lista = {}
+        for esto in r: 
+            try: lista[esto] += 1
+            except: lista[esto] = 1
+        real = []
+        para = 0
+        while True: 
+            if minimo > maximo: break
+            para = minimo + amplitud - 1
+            clase = {
+                'minimo': minimo, 
+                'maximo': para
+            }
+            minimo = para + 1
+            real.append(clase)
+        oficial = [{'clase': real}]
+        pprint(oficial)
+        oficial[0]['fi'] = []
+        for esto in real: 
+            numero = 0
+            for i in range(int(esto['minimo']), int(esto['maximo']) + 1): 
+                try: numero += lista[i]
+                except: continue
+            oficial[0]['fi'].append(numero)
+        parte = []
+        for esto in oficial[0]['fi']: 
+            if len(parte) >= 1: parte.append(esto + parte[-1])
+            else: parte.append(esto)
+        oficial[0]['fa'] = parte
+        oficial[0]['xi'] = list(map(lambda x: (x['minimo'] + x['maximo']) / 2, oficial[0]['clase']))
+    else: 
+        todos = list(map(lambda x: float(x), r))
+        lista = {}
+        for esto in todos: 
+            try: lista[esto] += 1
+            except: lista[esto] = 1
+        pprint(lista)
+        print(len(lista.items()))
+        return
     oficial[0]['fi.xi'] = [(xi * fi) for xi, fi in zip(oficial[0]['fi'], oficial[0]['xi'])]
     oficial[0]['fsr'] = list(map(lambda x: x / oficial[0]['fa'][-1], oficial[0]['fi']))
     oficial[0]['far'] = list(map(lambda x: x / oficial[0]['fa'][-1], oficial[0]['fa']))
@@ -110,7 +128,7 @@ def mostrar_tabla(todo : dict):
 
 # sns.histplot(
 #     diamonds,
-#     x="PrecioDolares", 
+#     x="PorcAlcohol", 
 #     # hue="TipoCerveza",
 #     # multiple="stack",
 #     # palette="light:m_r",
