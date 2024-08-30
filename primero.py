c206906bcca61fa8f4060861636e01ccc862bf80
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-08-30 10:25:45 -0400

    Con los fi correctos... creo, y con unos graficos correctos pero extraños

--- primero.py
+++ primero.py
@@ -8,8 +8,7 @@ from pprint import pprint
 
 sns.set_theme(style="ticks")
 
-# diamonds = sns.load_dataset("diamonds")
-diamonds = []
+cervezas_pandas = []
 cervezas = []
 
 with open('BEER_proyecto.csv', newline='') as csvfile:
@@ -22,22 +21,19 @@ with open('BEER_proyecto.csv', newline='') as csvfile:
         else: 
             esto['PorcAlcohol'] = float(esto['PorcAlcohol'])
         cervezas.append(esto)
-    diamonds = pd.DataFrame(cervezas)
+    cervezas_pandas = pd.DataFrame(cervezas)
 
 def precios(): 
-    todos = procedimiento('PrecioDolares')
-    mostrar_tabla(todos)
+    procedimiento('PrecioDolares')
 
 def calorias(): 
-    todos = procedimiento('Calorias')
-    # mostrar_tabla(todos)
+    procedimiento('Calorias')
 
 # Por la forma de datos usar datos no agrupados
 # El 0 para python lo considera inexistente por eso se le tuvo que hacer una
 # Pequeña modificación muy cerca del 0 
 def alcohol(): 
-    todos = procedimiento('PorcAlcohol')
-    # mostrar_tabla(todos)
+    procedimiento('PorcAlcohol')
 
 def procedimiento(busqueda): 
     r = list(map(lambda x: x[busqueda], cervezas))
@@ -51,6 +47,7 @@ def procedimiento(busqueda):
         rango = maximo - minimo 
         k = 1 + 3.3 * math.log(n, 10)
         amplitud = rango / k
+        print(f'Amplitud: {amplitud}')
         if amplitud > float(amplitud): 
             amplitud += 1
             amplitud = float(amplitud)
@@ -58,25 +55,40 @@ def procedimiento(busqueda):
         for esto in r: 
             try: lista[esto] += 1
             except: lista[esto] = 1
+        # pprint(lista)
         real = []
         para = 0
-        while True: 
-            if minimo > maximo: break
-            para = minimo + amplitud - 1
-            clase = {
-                'minimo': minimo, 
-                'maximo': para
-            }
-            minimo = para + 1
-            real.append(clase)
+        if amplitud < 1: 
+            while True: 
+                if minimo > maximo: break
+                para = minimo + amplitud 
+                clase = {
+                    'minimo': minimo, 
+                    'maximo': para
+                }
+                minimo = para + 0.01
+                real.append(clase)
+        else: 
+            while True: 
+                if minimo > maximo: break
+                para = minimo + amplitud - 1
+                clase = {
+                    'minimo': minimo, 
+                    'maximo': para
+                }
+                minimo = para + 1
+                real.append(clase)
         oficial = [{'clase': real}]
-        pprint(oficial)
+        # pprint(oficial)
         oficial[0]['fi'] = []
         for esto in real: 
             numero = 0
-            for i in range(int(esto['minimo']), int(esto['maximo']) + 1): 
-                try: numero += lista[i]
-                except: continue
+            llaves = list(filter(lambda x: x >= esto['minimo'] and x <= esto['maximo'], lista.keys()))
+            for una in llaves: 
+                numero += lista[una]
+            # for i in range(int(esto['minimo']), int(esto['maximo']) + 1): 
+            #     try: numero += lista[i]
+            #     except: continue
             oficial[0]['fi'].append(numero)
         parte = []
         for esto in oficial[0]['fi']: 
@@ -99,7 +111,10 @@ def procedimiento(busqueda):
     oficial[0]['fsr%'] = list(map(lambda x: x * 100, oficial[0]['fsr']))
     oficial[0]['far%'] = list(map(lambda x: x * 100, oficial[0]['far']))
     oficial[0]['fi.xi^2'] = [(xi * fixi) for xi, fixi in zip(oficial[0]['fi.xi'], oficial[0]['xi'])]
-    return oficial
+    mostrar_tabla(oficial)
+    plana = list(map(lambda x: x['minimo'], real))
+    plana.append(real[-1]['maximo']) 
+    diagramar(busqueda, plana)
 
 def mostrar_tabla(todo : dict): 
     todo = todo[0]
@@ -121,26 +136,27 @@ def mostrar_tabla(todo : dict):
         print(texto)
     print(len(texto) * '-')
 
-# print(diamonds)
+def diagramar(busqueda, lista): 
+    print(cervezas_pandas)
 
-# f, ax = plt.subplots(figsize=(7, 5))
-# sns.despine(f)
+    f, ax = plt.subplots(figsize=(7, 5))
+    sns.despine(f)
 
-# sns.histplot(
-#     diamonds,
-#     x="PorcAlcohol", 
-#     # hue="TipoCerveza",
-#     # multiple="stack",
-#     # palette="light:m_r",
-#     edgecolor=".3",
-#     linewidth=.5,
-#     log_scale=True,
-#     # discrete= True, 
-#     kde= True
-# )
-# ax.xaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
-# # ax.set_xticks([500, 1000, 2000, 5000, 10000])
-# plt.show()
+    sns.histplot(
+        cervezas_pandas,
+        x=busqueda, 
+        # hue="TipoCerveza",
+        # multiple="stack",
+        # palette="light:m_r",
+        edgecolor=".3",
+        linewidth=.5,
+        log_scale=True,
+        # discrete= True, 
+        kde= True
+    )
+    ax.xaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
+    ax.set_xticks(lista)
+    plt.show()
 
 def main(): 
     precios()
